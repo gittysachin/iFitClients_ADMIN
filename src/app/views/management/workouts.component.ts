85d159ed684c50d8e5c9554485b17577e68a0162
Sachin Jangid <34537025+gittysachin@users.noreply.github.com>
2020-07-29 13:53:59 +0530

    Assigning workout to users till error

--- src/app/views/management/workouts.component.ts
+++ src/app/views/management/workouts.component.ts
@@ -1,25 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { UsersService } from '../../services/users/users.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { HttpClient } from '@angular/common/http';
 import { DomSanitizer } from '@angular/platform-browser';
+import { UserRequestParams } from '../../models/users/user-request-params';
 import { Router } from '@angular/router';
+import { UserSearchParams } from '../../models/users/user-search-params';
+import * as data from '../../../assets/resources/users.json';
+import { HttpClient } from '@angular/common/http';
+import { DataBindingDirective } from '@progress/kendo-angular-grid';
+import { SecureAuth } from "../../helpers/secure-auth";
+import { process } from '@progress/kendo-data-query';
+import { employees } from './employees';
+import { images } from './images';
 
 @Component({
   selector: 'app-workouts',
   templateUrl: './workouts.component.html'
 })
 export class WorkoutsComponent implements OnInit {
-
+  @ViewChild(DataBindingDirective, { static: true }) dataBinding: DataBindingDirective;
   id: any;
   _title: string;
-  _userObject: any
+  _userObject: any;
+  _userRequestParams: UserRequestParams;
+  _lstUsers: any = (data as any).default;
+  userSearchParams: UserSearchParams;
+  modalReference: any;
   files: any = [];
+  itemsPerPage: number;
   workoutVideoChannel: Array<any> = [];
   videoURL: any = '';
+  _types: any;
+  _secureAuth: SecureAuth;
+  public gridData: any[];
+  public gridView: any[];
 
   constructor(private modalService: NgbModal,
     private _router: Router,
     private _http: HttpClient,
+    private _users: UsersService,
     private sanitizer: DomSanitizer) {
     const selectedClient = localStorage.getItem('selectedclient');
     const parsedClientJson = JSON.parse(selectedClient);
@@ -31,6 +50,21 @@ export class WorkoutsComponent implements OnInit {
       let sidebarChildElement = document.getElementById('child');
       sidebarChildElement.style.display = 'none';
     }
+    
+    this._userRequestParams = new UserRequestParams();
+    this.userSearchParams = new UserSearchParams();
+    this.itemsPerPage = this._userRequestParams.PageSize;
+    this._secureAuth = new SecureAuth();
+    // let sidebarRootElement = document.getElementById("root");
+    // sidebarRootElement.style.display = "block";
+    // let sidebarChildElement = document.getElementById("child");
+    // sidebarChildElement.style.display = "none";
+    this._types = [
+      { typeId: "40eec394-8d0f-426a-a43a-f4a55e3efea1", type: "Admin" },
+      { typeId: "51882a3c-33f9-4ff5-a721-4d1ba86430e2", type: "Trainer" },
+      { typeId: "5a9b19d7-09c2-4c88-8254-c19277896160", type: "Client" },
+      { typeId: "ae78a41e-f9a7-4bc9-b020-df5239be398f", type: "Super Admin" }
+    ];
   }
 
   ngOnInit() {
@@ -43,6 +77,116 @@ export class WorkoutsComponent implements OnInit {
       size: 'xl',
       centered: true
     });
+  }
+
+  getClients() {
+    const _controllerName = "users";
+    const _methodName = "user/by-type";
+    const _type = this._types.find(t => t.type === 'Trainer');
+    const user = JSON.parse(sessionStorage.user);
+    if (user) {
+      this._users
+        .getByType(_controllerName, _methodName, _type.typeId)
+        .subscribe((ut: any) => {
+          if(ut && ut.res) {
+            ut.res.map(data => { data.country = "US"; data.is_online = true; data.address = data.address1 + " " + data.address2; data.rating = 3; })
+            // this.gridView = ut.res;
+            this.gridView = ut.res;
+            console.log(ut.res)
+          }
+        });
+    }
+  }
+
+  onFilter(inputValue: string): void {
+    this.gridView = process(this.gridData, {
+      filter: {
+        logic: "or",
+        filters: [
+          {
+            field: 'full_name',
+            operator: 'contains',
+            value: inputValue
+          },
+          {
+            field: 'job_title',
+            operator: 'contains',
+            value: inputValue
+          },
+          {
+            field: 'budget',
+            operator: 'contains',
+            value: inputValue
+          },
+          {
+            field: 'phone',
+            operator: 'contains',
+            value: inputValue
+          },
+          {
+            field: 'address',
+            operator: 'contains',
+            value: inputValue
+          }
+        ],
+      }
+    }).data;
+
+    this.dataBinding.skip = 0;
+  }
+
+  AssignWorkout(modal: any) {
+    this.getClients();
+    this.modalReference = this.modalService.open(modal, {
+      size: 'lg',
+      centered: true
+    })
+  }
+
+  assignToUsers(usersListForm) {
+    if (usersListForm.form.status == "INVALID") {
+      Object.keys(usersListForm.controls).forEach(key => {
+        usersListForm.controls[key].markAsDirty();
+      });
+      return false;
+    }
+
+    // if (this.category.id) {
+    //   this.spinner.show();
+    //   let _controllerName = 'category';
+    //   this.catService.update(_controllerName, this.category).subscribe((cat: any) => {
+    //     this.modalReference.dismiss();
+    //     usersListForm.reset();
+    //     this.category = {
+    //       name: "",
+    //       id: '',
+    //       type: "Nutrition",
+    //       business_owner_id: "",
+    //       is_active: false
+    //     };
+    //     this.getAllCategories();
+    //     this.spinner.hide();
+    //   });
+    // } else {
+    //   this.spinner.show();
+    //   const userData = sessionStorage.getItem('user');
+    //   const userid = JSON.parse(userData);
+    //   this.category.business_owner_id = userid.id;
+    //   let _controllerName = 'category';
+    //   this.catService.save(_controllerName, this.category).subscribe((cat: any) => {
+    //     this.modalReference.dismiss();
+    //     usersListForm.reset();
+    //     this.category = {
+    //       name: "",
+    //       id: '',
+    //       type: "Nutrition",
+    //       business_owner_id: "",
+    //       is_active: false
+    //     };
+    //     this.getAllCategories();
+    //     this.spinner.hide();
+    //   });
+    // }
   }
 
   getAllWorkoutVideos() {
